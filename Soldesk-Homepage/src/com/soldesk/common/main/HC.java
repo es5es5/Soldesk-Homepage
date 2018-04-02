a413002c8e2209f3a9dc05e927990a4260748bac
es5es5 <es5e5s@nate.com>
2018-04-02 17:06:55 +0900

    디렉토리 재설정

--- Soldesk-Homepage/src/com/soldesk/common/main/HC.java
+++ Soldesk-Homepage/src/com/soldesk/common/main/HC.java
@@ -18,7 +18,7 @@ public class HC extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		request.getRequestDispatcher("jsp/index.jsp").forward(request, response);
+		request.getRequestDispatcher("index.jsp").forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
